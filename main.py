9b44c9168838f943d10538a7bc1b06e092387108
Walter <walter.dav.093@gmail.com>
2017-10-17 16:26:14 -0300

    fix calculo vector velocidad cuando z es negativo

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ particula = sphere(pos = (-5, 0, 0), radius = 0.5, color = color.cyan)
 paredDer = box(pos = (6, 0, 0), size = (0.2, 12, 12), color = color.green, opacity = 0.2)
 paredIzq = box(pos = (-6, 0, 0), size = (0.2, 12, 12), color = color.green, opacity = 0.2)
 
-paredSup = box(pos = (0, 6, 0), size = (12, 0.2, 12), color = color.green)
-paredInf = box(pos = (0, -6, 0), size = (12, 0.2, 12), color = color.green)
+paredSup = box(pos = (0, 6, 0), size = (12, 0.2, 12), color = color.green, opacity = 0.2)
+paredInf = box(pos = (0, -6, 0), size = (12, 0.2, 12), color = color.green, opacity = 0.2)
 
-paredTrasera = box(pos = (0, 0, -6), size = (12, 12, 0.2), color = color.green)
+paredTrasera = box(pos = (0, 0, -6), size = (12, 12, 0.2), color = color.green, opacity = 0.2)
 paredFrontal = box(pos = (0, 0, 6), size = (12, 12, 0.2), color = color.green, opacity = 0.01)
 
-particula.velocity = vector(25, 5, 5)
+particula.velocity = vector(25, 4, -6)
 
 deltaT = 0.005
 t = 0
@@ -18,7 +18,7 @@ escala = 0.1
 vectorVelocidad = arrow(pos = particula.pos, axis = escala * particula.velocity, color = color.yellow)
 
 #rastro de la particula
-particula.trail = curve(color=particula.color)
+particula.trail = curve(color=particula.color, retain = 50, interval = 20)
 
 while t < 100:
     rate(100)
@@ -34,7 +34,7 @@ while t < 100:
         vectorVelocidad.axis.y = -vectorVelocidad.axis.y
 
     #movimiento en Z limitado por  las paredes frontal y trasera
-    if particula.pos.z >= paredFrontal.pos.z or particula.pos.y < paredTrasera.pos.y:
+    if particula.pos.z >= paredFrontal.pos.z or particula.pos.z < paredTrasera.pos.z:
         particula.velocity.z = -particula.velocity.z
         vectorVelocidad.axis.z = -vectorVelocidad.axis.z
         
